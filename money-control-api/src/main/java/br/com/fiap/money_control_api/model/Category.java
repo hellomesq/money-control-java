947dfad7fe2940d1dbba925688a34592e1f96f32
hellomesq <helo.alvz@gmail.com>
2025-03-07 09:29:15 -0300

    delete_put

--- money-control-api/src/main/java/br/com/fiap/money_control_api/model/Category.java
+++ money-control-api/src/main/java/br/com/fiap/money_control_api/model/Category.java
@@ -12,22 +12,33 @@ public class Category {
         this.name = name;
         this.icon = icon;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getIcon() {
         return icon;
     }
+
     public void setIcon(String icon) {
         this.icon = icon;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", icon=" + icon + "]";
     }
 }
